03b4b46ea186217d858fa4787555e1812c519d6e
ClareChu <1062186165@qq.com>
2020-08-24 16:03:31 +0800

    ansible test

--- starter/es/client.go
+++ starter/es/client.go
@@ -26,7 +26,7 @@ func newClient() (client *Client) {
 
 func (c *Client) Connect(p *Properties) (err error) {
 	esUrl := fmt.Sprintf("http://%s:%d", p.Host, p.Port)
-	client, err := elastic.NewClient(
+	client, err := elastic.NewSimpleClient(
 		elastic.SetHealthcheckInterval(10*time.Second),
 		elastic.SetURL(esUrl),
 		elastic.SetBasicAuth(p.Username, p.Password),
